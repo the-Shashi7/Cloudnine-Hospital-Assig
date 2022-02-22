c6b3fff68a8e5318d1bd14924986b95e474ec04a
Shashi Bhushan <shashi.ic19@nsut.ac.in>
2022-02-22 20:31:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const SlideForEvery2Sec = (n)=>{
     for (let i = 0; i <= n; i++) {
         setTimeout( () =>{
-            console.log(i)
             const images = document.querySelector('.images').src =`img/${i}.jpg`
         }, i * 2000);
     }
